beb774e1726bdef6fffae20e410deefb21e37f97
khairin13chan <khairin13chan@gmail.com>
2023-01-31 22:09:58 +0800

    added topic seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserSeeder::class);
         $this->call(QuestionSeeder::class);
+        $this->call(TopicSeeder::class);
         $this->call(QuestionTypeSeeder::class);
     }
 }
